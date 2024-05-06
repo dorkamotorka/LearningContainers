df6bd2cb423b9e33405c481ba0b7e8692625452c
Ubuntu <teo.podobnik1234@gmail.com>
2024-05-06 09:16:09 +0200

    add freeze and unfreeze functionality

--- cgroups/main.go
+++ cgroups/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 	"os/exec"
 	"github.com/containerd/cgroups/v3/cgroup2"
 )
@@ -24,7 +25,7 @@ func main() {
 		CPU: &cgroup2.CPU{
 			//Weight: &weight, // e.g. (weight in the child cgroup) / (sum of cpu weights in the control groups) => percentage of cpu for this child cgroup processes
 			Max:    cgroup2.NewCPUMax(&quota, &period), // e.g. 200000 1000000 meaning processes inside this cgroup can (together) run on the CPU for only 0.2 sec every 1 second
-			//Cpus:   "0", // This limits on which CPU cores can the processes inside this cgroup run (NOTE: cpuset controller needs to be enabled!)
+			//Cpus:   "0", // This limits on which CPU cores can the processes inside this cgroup run (NOTE: Also "Mems" needs to be set: https://github.com/containerd/cgroups/blob/fa6f6841ed3d57355acadbc06f1d7ed4d91ac4f7/cgroup2/manager.go#L97!)
 			//Mems:   "0", // Memory Node” refers to an on-line node that contains memory. 
 		},
 		Memory: &cgroup2.Memory{
@@ -47,6 +48,7 @@ func main() {
 	
 	// dummy PID of -1 is used for creating a "general slice" to be used as a parent cgroup.
 	// see https://github.com/containerd/cgroups/blob/1df78138f1e1e6ee593db155c6b369466f577651/v2/manager.go#L732-L735
+	// "'-' inside the cgroup name make a child branch => my-cgroup-abc.slice - my.slice/my-cgroup.slice/my-cgroup-abc.slice/<processes>"
 	m, err := cgroup2.NewSystemd("/", "my-cgroup-abc.slice", -1, &res)
 	if err != nil {
 		log.Fatalln(err)
@@ -77,7 +79,18 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	log.Printf("%v", procs)
+	log.Printf("List of processes inside this cgroup: %v", procs)
+
+	log.Println("Freezing Process")
+	if err := m.Freeze(); err != nil {
+		log.Fatalln(err)
+	}
+	time.Sleep(time.Second * 15)
+
+	log.Println("Thawing Process")
+	if err := m.Thaw(); err != nil {
+		log.Fatalln(err)
+	}
 
 	// Wait for the command to finish
 	if err := cmd.Wait(); err != nil {
